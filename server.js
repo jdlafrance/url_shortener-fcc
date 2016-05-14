c746c0cf838a1175debd86a3b0fb34c2921ab527
JD Lafrance <jdavid.lafrance@gmail.com>
2016-05-14 17:46:30 +0000

    final-2

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
-var passport = require('passport');
+//var passport = require('passport');
 var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
+//require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -22,10 +22,10 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
-routes(app, passport);
+//routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
