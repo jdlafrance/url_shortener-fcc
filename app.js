67cc0b6078aca948235edde052184bc0a34998df
JD Lafrance <jdavid.lafrance@gmail.com>
2016-05-14 16:06:21 +0000

    final

--- app.js
+++ app.js
@@ -3,20 +3,33 @@ var mongo = require('mongodb').MongoClient;
 var url = require('url');
 var express = require('express');
 var app = express();
-var request = require('request');
 var regExp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 
 var shortURLPromise,
  short = require('short');
- 
-short.connect('mongodb://localhost/short');
+var dbdist = process.env.MONGOLAB_URI;
+var urlLocal = 'mongodb://localhost:27017/database';
+mongo.connect(dbdist, function(err, db){
+   if (err) {console.log('unable to connect. Error:', err);
+   } else {
+       console.log('connection established to', dbdist)
+   }
+   var collection = db.collection('short');
+
+short.connect(dbdist);
    short.connection.on('error', function(error){
     throw new Error(error);
 });
 
-app.get('/', function(req,res){
+app.get('/', function(req,res, next){
     var URL = url.parse(req.url).query;
-    if (regExp.exec(URL) != null){
+    collection.find({short_url: URL}).toArray(function(err, docs){
+        if (err) return;
+      if (docs[0] != undefined){
+          res.redirect('https://' + docs[0].original_url);
+          //db.close();
+          next();
+      } else if (regExp.exec(URL) != null){
     var URL1 = URL.replace('https://', '').replace('http://', '');
  var shortURLPromise = short.generate({URL : URL1});
  shortURLPromise.then(function(mongodbDoc){
@@ -25,9 +38,12 @@ app.get('/', function(req,res){
              'original_url': URL1,
              'short_url': result.hash
          };
+
+        collection.insert({original_url: URL1, short_url: obj.short_url});
+        res.send(obj);
+        next();
+        
          
-         
-         res.send(obj);
      }, function(error){
          if (error) throw new Error(error);
      });
@@ -39,8 +55,11 @@ app.get('/', function(req,res){
             'error': 'Wrong url format'
         };
         res.send(err);
+        next();
     }
  });
-
+});
+//db.close();
+});
 
 app.listen(8080);
