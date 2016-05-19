5bcea3ceccb2faf77840ae2cdc2c785817fdebde
JD Lafrance <jdavid.lafrance@gmail.com>
2016-05-19 14:08:35 +0000

    proc

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
-var short = require('short');
 
 var app = express();
 require('dotenv').load();
